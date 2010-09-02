f7658dc142cbadfea03e492c50054d9a0fbf84eb
Mark Connell <mark@markconnell.co.uk>
2010-09-02 19:18:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terse::Application.config.secret_token = 'cace53c918ba6cacef688db6a778c1b0e9082285bbf7fb59bd26b681386374162e0124c08ec5e9411c74204f2fcd78196eb4a76d285b546169f1f5d9914d408d'
