a25cd3568c2cfe4e622b5099a84ea30338173760
Mark Connell <mark@markconnell.co.uk>
2010-09-04 21:24:15 +0100

    Change private channel to a 'presence' channel.

--- app/controllers/pushers_controller.rb
+++ app/controllers/pushers_controller.rb
@@ -2,7 +2,7 @@ class PushersController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def auth
-    response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
+    response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {:user_id => Time.now.to_s})
     render :json => response
   end
 
