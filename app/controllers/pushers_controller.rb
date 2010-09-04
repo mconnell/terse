295c1de5ff8012ac14f77679168baaf8aa307fce
Mark Connell <mark@markconnell.co.uk>
2010-09-04 21:45:50 +0100

    Basic scoreboard.

--- app/controllers/pushers_controller.rb
+++ app/controllers/pushers_controller.rb
@@ -2,7 +2,7 @@ class PushersController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def auth
-    response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {:user_id => Time.now.to_s})
+    response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {:user_id => Time.now.to_i})
     render :json => response
   end
 
