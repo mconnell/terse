40b331c10080e23f018c698f6a27d310cd913d5d
Mark Connell <mark@markconnell.co.uk>
2010-09-05 12:14:59 +0100

    Don't push to the socket that sent the data.

--- app/controllers/sketches_controller.rb
+++ app/controllers/sketches_controller.rb
@@ -1,8 +1,9 @@
 class SketchesController < ApplicationController
 
   def create
+    socket_id       = params.delete('socket_id')
     params['paths'] = params.delete('path').map{|path| path.split(',')} if params['path']
-    Pusher["presence-game-#{params['game_id']}"].trigger('push', params)
+    Pusher["presence-game-#{params['game_id']}"].trigger('push', params, socket_id)
     render :text => 'ok'
   end
 
