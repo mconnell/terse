d7f83a3871461f82e6884c4b2d95ef461b303061
Mark Connell <mark@markconnell.co.uk>
2010-09-12 00:45:58 +0100

    Remove controllers that were used for ajax responses.

--- app/controllers/guesses_controller.rb
+++
@@ -1,9 +0,0 @@
-class GuessesController < ApplicationController
-
-  def create
-    socket_id = params.delete('socket_id')
-    Pusher["presence-game-#{params['game_id']}"].trigger('guess', params)
-    render :text => 'ok'
-  end
-
-end
