e5ad5e931df259152f0871a06b88946d706cdd51
Mark Connell <mark@markconnell.co.uk>
2010-09-12 22:57:44 +0100

    Utilise ws server path for channels.

--- lib/server.rb
+++ lib/server.rb
@@ -1,28 +1,31 @@
 require File.join File.dirname(__FILE__), '../config/environment'
 
 EventMachine.run do
-  @channel = EM::Channel.new
+  @channels = {}
 
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
       ws.onopen do
-        socket_id = @channel.subscribe { |msg| ws.send msg }
-        puts "#{socket_id} open"
+        channel_id = ws.request['Path'].split('/').last
+        channel = @channels[channel_id] ||= EM::Channel.new
+
+        socket_id = channel.subscribe { |msg| ws.send msg }
+        puts "open: channel #{channel_id}, socket #{socket_id}"
 
         ws.onmessage do |msg|
           event, data = JSON.parse(msg)
 
           case event
             when 'guess'
-              @channel.push msg
+              channel.push msg
             when 'draw'
               data['paths'] = data.delete('path[]').map{|path| path.split(',')} if data['path[]']
-              @channel.push [event, data].to_json
+              channel.push [event, data].to_json
           end
         end
 
         ws.onclose do
-          @channel.unsubscribe(socket_id)
-          puts "#{socket_id} closed"
+          channel.unsubscribe(socket_id)
+          puts "close: channel #{channel_id}, socket #{socket_id}"
         end
 
       end
