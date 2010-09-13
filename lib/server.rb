a4586a6a8430da462204c15eccd0750c8ce26d6a
Mark Connell <mark@markconnell.co.uk>
2010-09-13 22:41:40 +0100

    Every 65 seconds, clear a channel.

--- lib/server.rb
+++ lib/server.rb
@@ -3,10 +3,21 @@ require File.join File.dirname(__FILE__), '../config/environment'
 EventMachine.run do
   @channels = {}
 
+  def create_channel(channel_id)
+    @channels[channel_id] = EM::Channel.new
+
+    EventMachine::PeriodicTimer.new(65) do
+      puts "Clearing channel #{channel_id}"
+      @channels[channel_id].push ['clear', {}].to_json
+    end
+
+    @channels[channel_id]
+  end
+
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
       ws.onopen do
         channel_id = ws.request['Path'].split('/').last
-        channel = @channels[channel_id] ||= EM::Channel.new
+        channel = @channels[channel_id] || create_channel(channel_id)
 
         socket_id = channel.subscribe { |msg| ws.send msg }
         puts "open: channel #{channel_id}, socket #{socket_id}"
