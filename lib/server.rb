02a3b8fe6756511d59754fc05064dd8da2308583
Mark Connell <mark@markconnell.co.uk>
2010-09-24 23:18:53 +0100

    refactor find or create lobby

--- lib/server.rb
+++ lib/server.rb
@@ -3,10 +3,14 @@ require File.join File.dirname(__FILE__), '../config/environment'
 EventMachine.run do
   @lobbies = {}
 
+  def find_or_create_lobby(lobby_id)
+    @lobbies[lobby_id] || @lobbies[lobby_id] = Lobby.new(lobby_id)
+  end
+
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
     ws.onopen do
       lobby_id = ws.request['Path'].split('/').last
-      lobby = @lobbies[lobby_id] || Lobby.new(lobby_id)
+      lobby = find_or_create_lobby(lobby_id)
 
       socket_id = lobby.channel.subscribe { |msg| ws.send msg }
       puts "open: channel #{lobby_id}, socket #{socket_id}"
