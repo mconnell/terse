d8e5b1f699450065551794105a9498556963f128
Mark Connell <mark@markconnell.co.uk>
2010-10-19 23:17:13 +0100

    remove pusher.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
-gem "pusher", "0.6.0"
 gem "em-websocket", "0.1.4"
--- config/initializers/pusher.rb
+++
@@ -1,5 +0,0 @@
-require 'pusher'
-
-Pusher.app_id = '1855'
-Pusher.key    = 'e3c543914a4de3990f5a'
-Pusher.secret = 'fb1ba5d4254d591333b7'
