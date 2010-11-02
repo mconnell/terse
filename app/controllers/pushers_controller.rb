f675710ce76f3459e1534759ee32827aa984469e
Mark Connell <mark@markconnell.co.uk>
2010-11-02 22:58:13 +0000

    clean up.

--- app/controllers/pushers_controller.rb
+++
@@ -1,9 +0,0 @@
-class PushersController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-
-  def auth
-    response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {:user_id => session[:user_id]})
-    render :json => response
-  end
-
-end
