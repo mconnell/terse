253305bab27c7f2f504bc50ec0df3346cdd14309
Mark Connell <mark@markconnell.co.uk>
2010-11-02 23:10:38 +0000

    no need to setup a session variable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  before_filter :set_user_id
-
-  def set_user_id
-    session[:user_id] ||= Time.now.to_i
-  end
-
 end
